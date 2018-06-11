bc31707a5a682fd5d447ce2d2a59d4f34b8bb86a
Raymond Skinner <reqpro@requaos.com>
2018-06-11 09:11:58 -0400

    added two new patterns
    * recursive
    * iterative
    
    Implemented math/big, but it did not have the effect desired.

--- main.go
+++ main.go
@@ -1,32 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+)
+
+type iterativeState struct {
+	first, second int
+}
 
 func main() {
-	fmt.Println("hello")
+	fmt.Println("Fibonacci patterns:")
+
+	// Fraction derived and shaped with format library
 	twoDigit()
 	threeDigit()
+
+	// Impractical recursive calculation
+	for i := 0; i < 5; i++ {
+		fmt.Println(recursiveFibonacci(i))
+	}
+
+	// Slightly better iterative calculation
+	iterativeFibonacci(10)
+
 }
 
 func twoDigit() {
 	fmt.Println("two digits wide")
-	result := float64(1) / float64(9899)
+	result := new(big.Float).Quo(big.NewFloat(1), big.NewFloat(9899))
 	// only accurate up to 9 segments
 	// need to use math lib for accuracy
-	printItOut(result, 9, 2)
+	printItOut(*result, 12, 2)
 }
 
 func threeDigit() {
 	fmt.Println("three digits wide")
-	result := float64(1) / float64(998999)
+	result := new(big.Float).Quo(big.NewFloat(1), big.NewFloat(998999))
 	// only accurate up to 7 segments
 	// need to use math lib for accuracy
-	printItOut(result, 12, 3)
+	printItOut(*result, 12, 3)
 }
 
-func printItOut(in float64, n, wide int) {
+func printItOut(in big.Float, n, wide int) {
+	floater, _ := in.Float64()
 	for i := 0; i < n; i++ {
-		set := fmt.Sprintf("%."+fmt.Sprint((i+1)*wide)+"f", in)
+		set := fmt.Sprintf("%."+fmt.Sprint((i+1)*wide)+"f", floater)
 		fmt.Println(set[len(set)-wide:])
+	}
+}
+
+func recursiveFibonacci(n int) int {
+	if n == 1 || n == 0 {
+		return n
+	}
+	return recursiveFibonacci(n-1) + recursiveFibonacci(n-2)
+}
+
+func iterativeFibonacci(n int) {
+	fmt.Println(0)
+	v := iterativeState{0,1}
+	for i := 1; i < n; i++ {
+		fmt.Println(v.second)
+		v = iterativeState{v.second, v.first + v.second}
 	}
 }
